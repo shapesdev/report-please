4f45973f591123517ff39c6ddc1c21cf7e49d520
Shapsas <51415084+Shapsas@users.noreply.github.com>
2020-10-09 17:29:50 +0300

    Revert "Revert "Merge branch 'master' of https://github.com/Shapsas/Report_Please""
    This reverts commit ee1684add6cc25c582f0794b60b9a50a1f03c9b6.

--- Assets/Scripts/Classes/Scenario.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Scenario", menuName = "Scenario")]
-public class Scenario : ScriptableObject
-{
-
-}
